35b8a1ab5a5c29c2414b9b6bf5ea975289469f70
jemek27 <emajure27@gmail.com>
2025-05-12 11:33:26 +0200

    update

--- src/main/java/com/jemek/helloazure/controller/HelloController.java
+++ src/main/java/com/jemek/helloazure/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Azure!!! The webhook works!!!";
+        return "Hello from Azure!!! The webhook works!!! Testing update!!";
     }
 }
